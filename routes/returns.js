edadcc11250c79e10af995fd048d3a7e22575bd2
IsaacAnonymous <anonymoust438@gmail.com>
2022-12-02 21:54:09 +0800

    Testing "Rental Processed" & "Valid Request"

--- routes/returns.js
+++ routes/returns.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { Rental } = require('../modules/rental');
 const { logger } = require('../startup/logging');
+const auth = require('../middleware/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   logger.info(`Request data from test ${ req.body.customerId }`);
   console.log(`Request data from test ${ req.body.customerId }`);
 
   // console.log("[customerID: ] " + req.body.customerId);
   if (!req.body.customerId) return res.status(400).send('customerId not provided');
   if (!req.body.movieId) return res.status(400).send('movieId not provided');
+
 
   // TODO: access db
   const rental = await Rental.findOne({ 
@@ -18,9 +20,12 @@ router.post('/', async (req, res) => {
   });
   logger.info(`Response data from db ${ rental }`);
   if (!rental) return res.status(404).send("Rental not found");
+
+  // already return 
+  if (rental.dateReturned) return res.status(400).send('return already processed');
   
   
-  res.status(401).send('Unauthorized');
+  res.status(200).send();
 });
 
 module.exports = router;
